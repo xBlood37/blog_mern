252936015824155707fcca52e0e46c1156d820f1
xBlood37 <123d.123b@mail.ru>
2022-10-21 17:11:05 +0300

    finish maybe

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import mongoose from "mongoose";
 
 import {
@@ -37,6 +38,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
 app.post(
